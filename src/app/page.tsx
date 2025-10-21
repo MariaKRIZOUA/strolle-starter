cb36fdb2aba90db8785eb20cefa10fb8bd8ee88f
MariaKRIZOUA <krizouamaria@gmail.com>
2025-10-21 12:59:29 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{maxWidth: 900, margin: '0 auto', padding: 24}}>
+      <h1>Strollé — L’amitié, le temps du bien.</h1>
+      <p>On ne matche pas. <strong>On é.</strong></p>
+    </main>
+  );
+}
